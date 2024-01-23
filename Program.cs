9c556ba5d38bb735d656afac0a87edf8147c0b9b
Juan_C_Hernandez <33169379+JuanC-Hernandez@users.noreply.github.com>
2024-01-22 21:55:09 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace GA_ClassInstanceMethods_JuanHernandez
+{
+    internal class Program
+    {
+        //Juan Hernandez
+        // Guided Assignment: Class Instance Methods
+        // 01/22/2024
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
